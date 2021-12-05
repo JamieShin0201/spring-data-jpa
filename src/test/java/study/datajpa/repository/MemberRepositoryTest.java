ad96204ebbd261b4e3a8d4cc747e115ae33d31de
JamieShin0201 <jamieshin1450@gmail.com>
2021-12-05 19:28:39 +0900

    `@Query`, 레포지토리 메소드에 쿼리 정의하기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -78,4 +78,17 @@ public class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void findByUser() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("BBB", 20);
+        assertThat(result.get(0).getUsername()).isEqualTo("BBB");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
 }
