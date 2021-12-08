726937e5722f12dc3a0900e63051a43946798062
JamieShin0201 <jamieshin1450@gmail.com>
2021-12-08 12:17:39 +0900

    스프링 데이터JPA 벌크성 수정 쿼리 작성

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
@@ -34,4 +35,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByNames(@Param("names") List<String> names);
 
     Page<Member> findByAge(int age, Pageable pageable);
+
+    // 벌크성 쿼리를 실행하고 나서 영속성 컨텍스트 초기화
+    // 이 옵션 없이 회원을 다시 조회한다면 영속성 컨텍스트 과거 값이 남아서 문제가 될 수 있다.
+    // 만약 다시 조회해야한다면 꼭 영속성 컨텍스트를 초기화하자!
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
