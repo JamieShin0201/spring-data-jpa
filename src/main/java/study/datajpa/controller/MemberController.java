a0bd57bb058f49c8b441c127ff971f762f2aca3c
JamieShin0201 <jamieshin1450@gmail.com>
2021-12-11 22:19:29 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,6 +1,11 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +29,31 @@ public class MemberController {
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id") Member member) {
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
+    // 페이지 개별 설정
+    @GetMapping("/members-page")
+    public Page<Member> findMembers(
+            @PageableDefault(size = 12, sort = "username", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
+    // 접두사 활용
+    @GetMapping("/members-page2")
+    public Page<Member> findMembers2(
+            @Qualifier("member") Pageable memberPageable,
+            @Qualifier("order") Pageable orderPageable
+    ) {
+        Page<Member> page = memberRepository.findAll(memberPageable);
+        return page;
     }
 
     @PostConstruct
